08eee7d92d98b24b76d469df788c5ce0eb8cc529
current <161969964+current-meantime@users.noreply.github.com>
2025-04-29 16:57:36 +0200

    adjust update_dog_age.py to work with logging_config.py

--- app/services/update_dog_ages.py
+++ app/services/update_dog_ages.py
@@ -1,34 +1,10 @@
 import logging
-import os
 from sqlalchemy.orm import Session
 from app.models.dog import Dog
 from datetime import datetime, timedelta, timezone
 from sqlalchemy import select
-from sqlalchemy import func
 
-# Tworzenie katalogu 'logs', jeśli nie istnieje
-log_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'logs')
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
-# Inicjalizacja logera
-logger = logging.getLogger("update_logger")  # Use a specific logger name
-logger.setLevel(logging.INFO)
-
-# File handler
-file_handler = logging.FileHandler(os.path.join(log_dir, 'update.log'))
-file_handler.setLevel(logging.INFO)
-file_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
-
-# Console handler
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-console_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
-
-# Add handlers to the logger
-if not logger.handlers:  # Avoid adding handlers multiple times
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+logger = logging.getLogger(__name__)
 
 def update_dog_ages(db: Session):
     try:
@@ -42,13 +18,12 @@ def update_dog_ages(db: Session):
 
         logger.info(f"Found {len(dogs_to_update)} dogs to update.")
 
-        if dogs_to_update:
-            for dog in dogs_to_update:
-                dog.age += 1
+        for dog in dogs_to_update:
+            dog.age += 1
 
-            db.commit()
-            logger.info("Database commit successful.")
-            logger.info(f"Successfully updated ages for {len(dogs_to_update)} dogs.")
+        db.commit()
+        logger.info("Database commit successful.")
+        logger.info(f"Successfully updated ages for {len(dogs_to_update)} dogs.")
 
     except Exception as e:
-        logger.error(f"Error while updating dog ages: {str(e)}")
+        logger.error("Error while updating dog ages", exc_info=True)
