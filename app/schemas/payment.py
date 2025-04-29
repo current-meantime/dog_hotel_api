94103aacafccaf3d99750e70bbed601e60d22529
current <161969964+current-meantime@users.noreply.github.com>
2025-04-29 13:22:36 +0200

    add columns in owner schema

--- app/schemas/payment.py
+++ app/schemas/payment.py
@@ -4,7 +4,8 @@ from typing import Optional
 class PaymentCreate(BaseModel):
     stay_id: int
     is_paid: Optional[bool] = False
-    overdue_30_days: Optional[int] = 0
+    is_overdue: Optional[bool] = False
+    overdue_days: Optional[int] = 0
 
     class Config:
         from_attributes = True  # Zezwala na korzystanie z ORM
@@ -15,7 +16,8 @@ class PaymentRead(PaymentCreate):
 
 class PaymentUpdate(BaseModel):
     is_paid: Optional[bool] = None
-    overdue_30_days: Optional[int] = None
+    is_overdue: Optional[bool] = False
+    overdue_days: Optional[int] = 0
 
     class Config:
         from_attributes = True  # Zezwala na korzystanie z ORM
