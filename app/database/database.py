bad1d3bdf7a0d6cfa5e148ad05befedc585318df
current <161969964+current-meantime@users.noreply.github.com>
2025-04-29 15:50:00 +0200

    add load env and possibility of postgres to database.py

--- app/database/database.py
+++ app/database/database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+import os
 
+load_dotenv()  # Załadowanie zmiennych środowiskowych z pliku .env
 
-engine = create_engine("sqlite:///hotel_trial.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///hotel_trial.db")  # domyślnie SQLite, jeśli nie podano w .env
+
+engine = create_engine(DATABASE_URL)
+
 Session = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
