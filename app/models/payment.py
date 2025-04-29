90703f807a200aa3f0799e0c93f6a4a568d91f8c
current <161969964+current-meantime@users.noreply.github.com>
2025-04-29 17:23:29 +0200

    fix NoneType error in calculate_amount in PaymentModel

--- app/models/payment.py
+++ app/models/payment.py
@@ -1,7 +1,8 @@
 from app.database.database import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
-
+from app.models.stay import Stay as StayModel
+from sqlalchemy.orm import Session
 
 DAILY_RATE = 50.0  # Stawka za dzień pobytu
 
@@ -17,9 +18,14 @@ class Payment(Base):
     stay_id: Mapped[int] = mapped_column(ForeignKey("stays.id"), nullable=False)
     stay = relationship("Stay", back_populates="payments")
     
-    def calculate_amount(self):
-        stay_duration = (self.stay.end_date - self.stay.start_date).days
-        additional_fee = self.stay.additional_fee_per_day * stay_duration
-        base_fee = DAILY_RATE * stay_duration
-        total_amount = base_fee + additional_fee
-        return total_amount
+    def calculate_amount(self, db: Session) -> float:
+        stay = db.get(StayModel, self.stay_id)
+        if not stay:
+            raise ValueError(f"Stay not found for stay_id={self.stay_id}")
+        
+        #TODO: Add validation for stay dates/duration in other files and remove this if it gets redundant
+        stay_duration = (stay.end_date - stay.start_date).days + 1  # +1 to include the last day
+        if stay_duration <= 0:
+            raise ValueError("Stay duration must be greater than 0 days.")
+        additional_fee = stay.additional_fee_per_day if stay.additional_fee_per_day else 0
+        return stay_duration * (DAILY_RATE + additional_fee)
